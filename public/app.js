ae12196ae76d060d3e989bc8089af7549496ceae
Narek Tutikyan <narek@king.am>
2017-01-23 15:56:51 +0400

    git init

---
+++ public/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+
+angular.module('kohar', [
+  'ngRoute',
+  'ngAnimate',
+  'ngTouch',
+  'ngFileUpload',
+  'kohar.services',
+  'kohar.photo-album',
+  'kohar.photo',
+  'kohar.photoDir',
+  //'kohar.timeConverter',
+  'kohar.previewImage',
+  'kohar.version',
+  'kohar.version',
+  'ui.bootstrap',
+  'ui.grid',
+  'ui.grid.pagination',
+  'ui.grid.edit',
+  'ui.grid.resizeColumns',
+  'ui.grid.validate',
+  'ui.grid.selection',
+  'kohar.music-albums',
+  'kohar.music',
+  'kohar.video',
+  'kohar.concerts',
+  'ui.bootstrap.datepickerPopup'
+
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/music-albums'});
+}]).controller('MainCtrl', function($scope, myConfig){
+    $scope.activeMenu = 'music-albums';
+
+  console.log('MainCtrl');
+
+  console.log(myConfig);
+});
